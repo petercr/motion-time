be314e8044648a6693e37e785ef043345ac575ad
Peter Cruckshank <petecruckshank@gmail.com>
2020-03-14 20:23:59 -0400

    brought in Framer Motion & got DIV's moving!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { motion } from "framer-motion";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <motion.div
+          animate={{ rotate: 360, opacity: 1 }}
+          transition={{ duration: 3 }}
+          initial={{ opacity: 0.25 }}
+          whileHover={{ scale: 1.2, duration: 0.5 }}
+          whileTap={{ scale: 0.8, duration: 0.5 }}
+          drag="x"
+          dragConstraints={{ left: -250, right: 250 }}
+        >
+          <img src={logo} className="App-logo" alt="logo" />
+        </motion.div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
