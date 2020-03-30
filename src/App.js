19a8524e0abf7ca493af8bcaffab599a50bad589
Peter Cruckshank <petecruckshank@gmail.com>
2020-03-29 20:52:58 -0400

    chnaged the animation to a Yoyo with Bézier curve Also added useRef() to make the #App to restraint

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import { motion } from "framer-motion";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const constraintsRef = useRef(null);
   return (
-    <div className="App">
+    <div className="App" ref={constraintsRef}>
       <header className="App-header">
         <motion.div
           animate={{ rotate: 360, opacity: 1 }}
-          transition={{ duration: 3 }}
+          transition={{ yoyo: Infinity, ease: "easeInOut", duration: 2 }}
           initial={{ opacity: 0.25 }}
-          whileHover={{ scale: 1.2, duration: 0.5 }}
-          whileTap={{ scale: 0.8, duration: 0.5 }}
+          whileHover={{ scale: 1.25 }}
+          whileTap={{ scale: 0.5, duration: 0.5 }}
           drag="x"
-          dragConstraints={{ left: -250, right: 250 }}
+          dragConstraints={constraintsRef}
         >
           <img src={logo} className="App-logo" alt="logo" />
         </motion.div>
