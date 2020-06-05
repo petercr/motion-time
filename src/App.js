db71d2552fe46d01f75e304cc09109c40a496059
Peter Cruckshank <petecruckshank@gmail.com>
2020-06-04 21:02:04 -0400

    set files back to 2nd commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
-import React, { useRef } from "react";
-import { motion } from "framer-motion";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const constraintsRef = useRef(null);
   return (
-    <div className="App" >
-      <header className="App-header" ref={constraintsRef}>
-        <motion.div
-          animate={{ rotate: 360, opacity: 1 }}
-          transition={{ yoyo: Infinity, ease: "easeInOut", duration: 2 }}
-          initial={{ opacity: 0.25 }}
-          whileHover={{ scale: 1.25 }}
-          whileTap={{ scale: 0.5, duration: 0.5 }}
-          drag
-          dragConstraints={constraintsRef}
-        >
-          <img src={logo} className="App-logo" alt="logo" />
-        </motion.div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Feel free to check out the source code, I hope you all had lots of fun <span role="img" aria-label="a party popper shooting ribbons">🎉</span>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://github.com/petercr/motion-time"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Source Code on GitHub
+          Learn React
         </a>
       </header>
     </div>
